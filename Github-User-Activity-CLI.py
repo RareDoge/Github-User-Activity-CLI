5eef0de6ada09cb09e19b81b6f81efe0837d07b0
RareDoge <corey5564@gmail.com>
2026-04-01 12:32:48 -0700

    Another git commit to test features

--- Github-User-Activity-CLI.py
+++ Github-User-Activity-CLI.py
@@ -1,6 +1,29 @@
+import requests
 
-# Test 1
+# Fetch Github Data using Github CLI
 
-# testing git commit and push
-# trying to understand how git and github work.
-print("this is a test")
+url = "https://api.github.com/users/RareDoge/events"
+
+params = {
+    "login" : "RareDoge"
+}
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    repo_commits = {}
+    commits = 0
+
+    for event in data:
+        repo_name = event['repo']['name']
+        if event['type'] == 'PushEvent':
+            commits += 1
+        if repo_name in repo_commits:
+            repo_commits[repo_name] += commits
+        else:
+            repo_commits[repo_name] = commits
+        commits = 0
+    print(repo_commits)
+else:
+    print("keep working...")
